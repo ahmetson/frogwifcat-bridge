5f0fd02d9aeadc6bb228c3b6bcd8261c24ecfecb
Medet Ahmetson <milayter@gmail.com>
2024-06-13 21:57:32 +0700

    increase gas limit to 210k

--- src/business/blockchain/oft/useEstimateSendFee.ts
+++ src/business/blockchain/oft/useEstimateSendFee.ts
@@ -24,13 +24,8 @@ export type SendParam = {
 
 export function useEstimateSendFee({ formState }: IParams) {
   const _options = Options.newOptions();
-  console.log(`Chain id == ${formState.destinationChain?.id}, is it base? ${formState.destinationChain?.id == 8453}, network name ${formState.destinationChain?.name}`);
-  if (formState.destinationChain?.id == 8453) {
-  _options.addExecutorLzReceiveOption(1000000, 0);
-  }
-  else {
-    _options.addExecutorLzReceiveOption(70000, 0);
-  }
+  console.log(`Chain id == ${formState.destinationChain?.id}, adding 21,000 GAS limit`);
+  _options.addExecutorLzReceiveOption(210000, 0);
   let lzEndpoint = parseInt(formState.destinationChain?.lzEndpointId as string);
   console.log(`To: ${bytesToHex(hexToBytes(formState.recipient, { size: 32 }))}, ${hexToBytes("0x00", {size: 32})}`);
   const { data, status, error } = useReadContract({
